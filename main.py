4de01be863193504a343565f27cf11141c1b5292
Robert Griffin Stober <stoberr@oregonstate.edu>
2024-10-31 18:40:29 -0700

    Library done except displaying books

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# import time
-# import random
+import math
 
 
 library_text = r"""
@@ -103,7 +102,7 @@ class Database:
     self.num_books = int(file.readline())
     self.books = []
     for x in range(self.num_books):
-        self.books.append(Book(file.readline(),file.readline(),file.readline(),file.readline(),file.readline()))
+        self.books.append(Book(file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline()))
     file.close()
     file = open(path2, 'r')
     self.settings = []
@@ -123,6 +122,8 @@ class Database:
         file.write(self.books[x].date)
         file.write(self.books[x].rating)
         file.write(self.books[x].review)
+        file.write(self.books[x].progress)
+        file.write(self.books[x].status)
     file.write("\n")
     file.close()
 
@@ -136,6 +137,7 @@ class Database:
     file = open(self.settings_path, 'w')
     file.writelines([str(self.settings[0])+"\n",str(self.settings[1])+"\n",str(self.settings[2])+"\n",str(self.settings[3])+"\n",str(self.settings[4])+"\n",str(self.settings[5])+"\n",str(self.settings[6])+"\n",str(self.settings[7])+"\n",str(self.settings[8])+"\n",str(self.settings[9])+"\n",str(self.settings[10])+"\n",str(self.settings[11])+"\n",str(self.settings[12])+"\n",str(self.settings[13])])
     file.close
+
 
   def print_settings(self):
       if(self.settings[0]==0):
@@ -196,14 +198,35 @@ class Database:
         string4="20"
       else:
         string4="[20]"
-      print(f"(S): Books per page   {string1} {string2} {string3} {string4}")
+      print(f"(B): Books per page   {string1} {string2} {string3} {string4}")
+
+  def get_page_size(self):
+     if self.settings[10]==1:
+        return 5
+     if self.settings[11]==1:
+        return 10
+     if self.settings[12]==1:
+        return 15
+     if self.settings[13]==1:
+        return 20
+  def sort(self):
+     if self.settings[4]==1:
+        database.books.sort(key=lambda x: x.progress, reverse=True)
+     if self.settings[5]==1:
+        database.books.sort(key=lambda x: x.title, reverse=True)
+     if self.settings[6]==1:
+        database.books.sort(key=lambda x: x.author, reverse=True)
+     if self.settings[7]==1:
+        database.books.sort(key=lambda x: x.date, reverse=True)
 class Book:
-  def __init__(self, title, author, date, rating, review):
+  def __init__(self, title, author, date, rating, review, progress, status):
     self.title = title
     self.author = author
     self.date = date
     self.rating = rating
     self.review = review
+    self.progress = progress
+    self.status = status
   def set_default(self):
     if (self.title ==None):
         self.title = "Unset Title"
@@ -215,6 +238,10 @@ class Book:
         self.rating = "Unknown Rating"
     if (self.review ==None):
         self.review = "Empty Review"
+    if (self.progress ==None):
+        self.progress = "0.00"
+    if (self.status ==None):
+        self.status = "Unread"
 
 def read_book(database):
     repeat=0
@@ -241,7 +268,7 @@ def read_book(database):
             print(f"No book found at {file_path} \n Please try again")
     repeat =0
     file = open(file_path, 'r')
-    book = Book(file.readline(),file.readline(),file.readline(),file.readline(),file.readline())
+    book = Book(file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline())
     file.close()
     while(repeat==0):
         NULL_VAL = input("Would you like to replace any NULL values with defaults?(Y/N): WARNING, leaving NULL values in can have negative effects. It's recomended to choose Y\n:")
@@ -260,7 +287,6 @@ def read_book(database):
 def settings(database):
     repeat=0
     while(repeat == 0):
-        # Print(.....)
         print(settings_text)
         database.print_settings()
         user_input = input("Type the () letter followed by the number you wish to activate (i.e type S1 to set the sorting order to your progress in reading each book). Type E to exit the settings. type RESET to change the settings to default.\n: ")
@@ -293,9 +319,26 @@ def settings(database):
             
 
 
+def library(database):
+    repeat=0
+    current_page=1
+    while(repeat == 0):
+        print(library_text)
+        pages=int(math.ceil(float(database.num_books)/database.get_page_size()))
+        database.sort()
+        #print books
 
 
+        
+        print(F"Page {current_page} of {pages}")
+        user_input = input("Type P followed by a number to change the page.\nType L to add books. Type S to go to settings\n:")
+        if(user_input=="L"):
+            read_book(database)
+        elif(user_input=="S"):
+            settings(database)
 
+    return None
+   
 
 
 
@@ -303,8 +346,6 @@ def settings(database):
 
 
 database= Database("C:\\Users\\tebbu\\Desktop\\CS\\CS361\\main\\database.txt","C:\\Users\\tebbu\\Desktop\\CS\\CS361\\main\\settings.txt")
-
-
-settings(database)
+library(database)
 
 # read_book(database)
